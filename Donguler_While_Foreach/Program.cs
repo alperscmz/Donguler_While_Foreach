72e6b58851cdbfb67f7a945cddda9c8f60eb01ff
alperscmz <alpersacmaozu@gmail.com>
2022-01-23 12:05:19 +0300

    Donguler_While_Foreach
    vsc c# ile Donguler_While_Foreach

---
+++ Donguler_While_Foreach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Donguler_While_Foreach
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            //1'den başlayıp konsoldan girilen sayıya kadar olan sayıların ortalamasını yazdıran program.
+        Console.WriteLine("Sayı giriniz: ");
+        int sayi=Convert.ToInt32(Console.ReadLine());
+        int sayac=1;
+        int toplam=0;
+        
+           while (sayac<=sayi)
+           { 
+              toplam=toplam+sayac;
+              sayac++;  
+           } 
+           Console.WriteLine(toplam/sayi);
+       
+       //a' dan z'ye kadar harfleri yazdıran program.
+        char character='a';
+        while(character<='z')
+        {
+            Console.Write(character);
+            character++;
+        }
+        //foreach döngüsü ile dizi döndürme. 
+         string[] arabalar={"BMW","MERCEDES","AUDI","OPEL","TOYOTA"};
+         foreach (var araba in arabalar)
+          {
+          Console.WriteLine(araba); 
+          }
+          } 
+      }
+}
